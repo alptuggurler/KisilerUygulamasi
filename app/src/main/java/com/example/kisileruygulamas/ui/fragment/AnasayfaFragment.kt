5e23e28b18ddc90b800651ca797525d7b7248456
Alptuğ Gürler <alptug.gurler@gmail.com>
2023-12-11 14:48:42 +0300

    v1.3 Kişiler Sayfasına Arama Eklendi kart tasarıımı oluşturuldu Kişiler Düzenleme Sayfasına geçiş eklendi Silme Fonksyonu yazıldı

--- app/src/main/java/com/example/kisileruygulamas/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/example/kisileruygulamas/ui/fragment/AnasayfaFragment.kt
@@ -1,14 +1,21 @@
 package com.example.kisileruygulamas.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView.OnQueryTextListener
+
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.kisileruygulamas.R
 import com.example.kisileruygulamas.data.entity.Kisiler
 import com.example.kisileruygulamas.databinding.FragmentAnasayfaBinding
+import com.example.kisileruygulamas.ui.adapter.KisilerAdapter
 
 class AnasayfaFragment : Fragment() {
 
@@ -16,19 +23,46 @@ class AnasayfaFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        binding.rv.layoutManager = LinearLayoutManager(requireContext())
+        binding.toolbarAnasayfa.title = "Kişiler"
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1,"Ahmet","1111")
+        val k2 = Kisiler(2,"Mehmet","2222")
+        val k3 = Kisiler(3,"Ayşe","3333")
+        val k4 = Kisiler(4,"Fatma","4444")
+
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+        kisilerListesi.add(k4)
+
+        val kisilerAdapter = KisilerAdapter(requireContext(),kisilerListesi)
+
+        binding.rv.adapter = kisilerAdapter
+
+
         binding.fab.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.kisiKayitFragment)// it yerine binding.fab yazılabilr
         }
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                ara(newText)
+                return true
+            }
 
-        binding.buttonDetay.setOnClickListener {
-            val kisi = Kisiler(1,"ahmet","1111")
-            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi = kisi)
-            Navigation.findNavController(it).navigate(gecis)
-        }
+            override fun onQueryTextSubmit(query: String): Boolean {
+                ara(query)
+                return true
+            }
+        })
+
 
 
         return binding.root
 
     }
-
+    fun ara(aramaKelimesi: String){
+        Log.e("Kişi Ara",aramaKelimesi)
+    }
 }
